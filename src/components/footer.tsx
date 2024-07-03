5abe883b8469d5e0ee6fed83d04b345276144915
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-07-03 08:02:09 +0000

    chore: Add footer component with styling and media query updates

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className={styles.footer}>出典：RESAS（地域経済分析システム）</p>
+    </footer>
+  );
+};
+
+export default Footer;
